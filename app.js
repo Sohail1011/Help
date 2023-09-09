055ab3127b395c8670c9d9c101137a3b4eba8b11
Sohail Iran <aratavalga@gmail.com>
2023-09-08 22:02:31 -0500

    go

---
+++ app.js
@@ -0,0 +1,16 @@
+const res = prompt('Inserte su edad');
+
+if (res => 18) {
+    console.log('Usted es mayor de edad.');
+} else {
+    console.log('Usted es menor de edad.');
+}
+
+
+const capital = prompt('La capital de Perú');
+
+if (capital === 'Lima') {
+    console.log('Aquí nadie gano.');
+} else {
+    console.log('Todo se pierde.');
+}
